882bca84aa76fa685d9f5d11cd5c8cdd40f02b34
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2020-08-18 17:05:07 +0000

    uploaded logical file

---
+++ logical.py
@@ -0,0 +1,11 @@
+a=1
+b=3
+if(a==1 and b==3):
+		print("a is smaller")
+else:
+		print("b is less than or equal to a")
+if(a<b or a==b):
+		print("a is less than or equal to b")
+else:
+		print("a is larger than b")
+print(not(1))
