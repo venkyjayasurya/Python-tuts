efb1faf9121943ef168ee75d7fa955c632299343
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2020-08-19 15:34:04 +0000

    m and i operators

---
+++ mandioperator.py
@@ -0,0 +1,16 @@
+#Membership and Identity Operator
+# in, not in, is, is not 
+a=[10,20,30,40,50]
+b=25
+c=25
+print(20 in a) #t
+print(b in a) #f
+
+print(20 not in a) #f
+print(b not in a) #t
+
+print(a is b) #f
+print(b is c) #t
+
+print(a is not b) #t
+print(b is not c) #f
