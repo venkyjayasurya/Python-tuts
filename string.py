47b9c13d31b668394a9f580632d6d400384b1c24
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2020-08-22 17:58:59 +0000

    string manipulation

---
+++ string.py
@@ -0,0 +1,7 @@
+a="Hello! CSE"
+b="How are you?"
+print(a)	#print entire string
+print(a[0:4])	#print first four characters
+print(a[5])	#print sixth character
+print(a*2)	#print the string 2 times
+print(a+"\n"+b)	#concatenate two strings
