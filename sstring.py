da9ad9bfd6f6409d2bd181b11ec35044d1ce2dec
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2020-08-22 17:58:28 +0000

    string manipulation

---
+++ sstring.py
@@ -0,0 +1,11 @@
+print(r"Hello!\nPython")
+
+#  The below string is a docstring. It is same as comment
+
+"""The quick brown fox
+jumped over the lazy
+dogs"""
+
+print("""The quick brown fox
+jumped over the lazy
+dogs""") 
