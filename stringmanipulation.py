6e8e74df364e1d409a97a79d3b850c1a0f1f4795
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2020-08-22 17:59:50 +0000

    string manipulation

---
+++ stringmanipulation.py
@@ -0,0 +1,13 @@
+#string Manipulation
+
+str1="Hello!"
+str2="VJIT"
+str3=str1+str2					#concatenation
+print("The Concatenation string is: ",str3)
+
+str4="Hello! "
+str5=input("Enter the Name : ")
+str4=str4+str5				 	#appending
+str4=str4+". Welcome to Python Programming."	#appending literal
+print(str4)
+print(str1*3)					#multiplying
